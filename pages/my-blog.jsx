7f1f1cd0803324b50bd0c5dad509ef4297b18efb
Ivan <ivan.ivanov.freelancer@gmail.com>
2023-06-13 19:47:50 +0300

    cities.js

--- pages/my-blog.jsx
+++ pages/my-blog.jsx
@@ -1,7 +1,20 @@
 // my-blog.jsx
 
+import cities from "../src/data/cities";
+
 const MyBlog = () => {
-  return <div>Блог</div>;
+  return (
+    <div>
+      <h1>Блог</h1>
+      <ul>
+        {cities.map((city, index) => (
+          <li key={index}>
+            {city.city}, {city.region}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MyBlog;
