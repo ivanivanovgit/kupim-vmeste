9f495b1ea191f17704958093987fab53f0020d1b
Ivan <ivan.ivanov.freelancer@gmail.com>
2023-06-25 14:49:48 +0300

    StyledAccordionSummary.js rotate(-90deg)

--- src/components/Auxiliary/StyledAccordionSummary.js
+++ src/components/Auxiliary/StyledAccordionSummary.js
@@ -3,10 +3,14 @@
 import { AccordionSummary } from "@mui/material";
 import { styled } from "@mui/system";
 
-export const StyledAccordionSummary = styled(AccordionSummary)(({ theme }) => ({
+export const StyledAccordionSummary = styled(AccordionSummary)(() => ({
   flexDirection: "row-reverse",
   justifyContent: "space-between",
-  "& .MuiAccordionSummary-expandIcon": {
+  "& .MuiAccordionSummary-expandIconWrapper": {
+    transform: "rotate(-90deg)",
     marginRight: "auto",
+  },
+  "&.Mui-expanded .MuiAccordionSummary-expandIconWrapper": {
+    transform: "rotate(0deg)",
   },
 }));
