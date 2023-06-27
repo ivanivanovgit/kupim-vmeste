e2709c2aec2375adb85b25e7b27b2446fe41b773
Ivan <ivan.ivanov.freelancer@gmail.com>
2023-06-27 20:21:20 +0300

    InstructionAddMarkerChatMap.jsx

---
+++ pages/InstructionAddMarkerChatMap.jsx
@@ -0,0 +1,71 @@
+// InstructionAddMarkerChatMap.jsx
+
+import Image from "next/image";
+import addMessage from "../public/images/add-message.gif";
+import delMessage from "../public/images/delete-message.gif";
+import { Constants } from "../src/CONSTANTS";
+import stylesHome from "../styles/Help.module.scss";
+
+function InstructionAddMarkerChatMap() {
+  return (
+    <>
+      <div className={stylesHome.subsectionTitle}>Вкладка "Чат на карте"</div>
+      <div className={stylesHome.text}>
+        Вкладка "Чат на карте" позволяет обмениваться сообщениями, размещая их
+        на карте. Так как сервис "Купим вместе" разработан без необходимости
+        регистрации для удобства использования, то сообщения пользователей на
+        вкладке " Чат на карте" становятся видимыми для всех сразу после их
+        добавления, и каждый может удалить ваше сообщение, поскольку все они
+        находятся в общем доступе. Основные функции включают:
+      </div>
+      <ul className={stylesHome.bulletList}>
+        <li>
+          Добавление тем: Пользователи могут создавать темы для обсуждения и
+          добавлять маркеры с сообщениями.
+        </li>
+        <li>
+          Удаление тем: Можно удалить тему, но только после удаления всех
+          связанных с ней маркеров.
+        </li>
+        <li>
+          Выбор тем: Пользователи могут выбрать из существующих тем. Если тема
+          не выбрана, система предупреждает об этом.
+        </li>
+        <li>
+          Просмотр маркеров по темам: Предоставляет возможность просмотреть все
+          маркеры, связанные с выбранной темой.
+        </li>
+        <li>
+          Добавление сообщений: Для этого нужно выбрать тему и ввести текст
+          сообщения, который затем отображается на карте.
+        </li>
+      </ul>
+      <div className={stylesHome.imageContainer}>
+        <Image
+          src={addMessage}
+          alt="Чат на карте: добавление сообщения"
+          width={Constants.widthInstMessChat}
+          height="auto"
+          priority
+        />
+        <div className={stylesHome.imageCaption}>
+          Добавление сообщения на карту
+        </div>
+      </div>
+      <div className={stylesHome.imageContainer}>
+        <Image
+          src={delMessage}
+          alt="Чат на карте: просмотр и удаление сообщения с карты "
+          width={Constants.widthInstMessChat}
+          height="auto"
+          priority
+        />
+        <div className={stylesHome.imageCaption}>
+          Удаление сообщений с карты
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default InstructionAddMarkerChatMap;
--- pages/help.jsx
+++ pages/help.jsx
@@ -1,79 +1,16 @@
 // help.jsx
 
-import addMessage from "../public/images/add-message.gif";
-import delMessage from "../public/images/delete-message.gif";
-import Image from "next/image";
-import { Constants } from "../src/CONSTANTS";
 import stylesHome from "../styles/Help.module.scss";
 import Accordion from "../src/components/Auxiliary/Accordion";
+import ActiveLink from "../src/components/Auxiliary/ActiveLink";
 
 const Help = () => {
   return (
     <div className={stylesHome.container}>
       Помощь
-      <Accordion
-        id="accordion-1"
-        title={
-          <div className={stylesHome.subsectionTitle}>
-            Вкладка "Чат на карте"
-          </div>
-        }
-      >
-        <div className={stylesHome.text}>
-          Вкладка "Чат на карте" позволяет обмениваться сообщениями, размещая их
-          на карте. Так как сервис "Купим вместе" разработан без необходимости
-          регистрации для удобства использования, то сообщения пользователей на
-          вкладке " Чат на карте" становятся видимыми для всех сразу после их
-          добавления, и каждый может удалить ваше сообщение, поскольку все они
-          находятся в общем доступе. Основные функции включают:
-        </div>
-        <ul className={stylesHome.bulletList}>
-          <li>
-            Добавление тем: Пользователи могут создавать темы для обсуждения и
-            добавлять маркеры с сообщениями.
-          </li>
-          <li>
-            Удаление тем: Можно удалить тему, но только после удаления всех
-            связанных с ней маркеров.
-          </li>
-          <li>
-            Выбор тем: Пользователи могут выбрать из существующих тем. Если тема
-            не выбрана, система предупреждает об этом.
-          </li>
-          <li>
-            Просмотр маркеров по темам: Предоставляет возможность просмотреть
-            все маркеры, связанные с выбранной темой.
-          </li>
-          <li>
-            Добавление сообщений: Для этого нужно выбрать тему и ввести текст
-            сообщения, который затем отображается на карте.
-          </li>
-        </ul>
-        <div className={stylesHome.imageContainer}>
-          <Image
-            src={addMessage}
-            alt="Чат на карте: добавление сообщения"
-            width={Constants.widthInstMessChat}
-            height="auto"
-            priority
-          />
-          <div className={stylesHome.imageCaption}>
-            Добавление сообщения на карту
-          </div>
-        </div>
-        <div className={stylesHome.imageContainer}>
-          <Image
-            src={delMessage}
-            alt="Чат на карте: просмотр и удаление сообщения с карты "
-            width={Constants.widthInstMessChat}
-            height="auto"
-            priority
-          />
-          <div className={stylesHome.imageCaption}>
-            Удаление сообщений с карты
-          </div>
-        </div>
-      </Accordion>
+      <ActiveLink href="/InstructionAddMarkerChatMap">
+        Добавление маркера
+      </ActiveLink>
       <Accordion
         id="accordion-2"
         title={
