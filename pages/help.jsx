3fe0aa60266fa4c8c7bb591d8e4c1a925c2ff5c1
Ivan <ivan.ivanov.freelancer@gmail.com>
2023-06-29 16:30:21 +0300

    InstructionChatMap.js

--- pages/help.jsx
+++ pages/help.jsx
@@ -14,11 +14,29 @@ const Help = () => {
           <li>Раздел "Чат на карте"?</li>
         </ActiveLink>
         <ol className={stylesHome.numberedSubListHelp}>
+          <ActiveLink href="/help#search-adress">
+            <li>Выбор адреса для маркера</li>
+          </ActiveLink>
+          <ActiveLink href="/help#choice-theme">
+            <li>Выбор темы</li>
+          </ActiveLink>
+          <ActiveLink href="/help#adding-theme">
+            <li>Добавление темы</li>
+          </ActiveLink>
+          <ActiveLink href="/help#deleting-theme">
+            <li>Удаление темы</li>
+          </ActiveLink>
+          <ActiveLink href="/help#show-all-markers">
+            <li>Показать маркеры по всем темам</li>
+          </ActiveLink>
           <ActiveLink href="/help#add-marker">
-            <li>Добавление маркера на карту</li>
+            <li>Добавление маркера с сообщением на карту</li>
           </ActiveLink>
           <ActiveLink href="/help#delete-marker">
-            <li>Удаление маркера с карты</li>
+            <li>Просмотр и удаление сообщений с карты</li>
+          </ActiveLink>
+          <ActiveLink href="/help#share-marker">
+            <li>Поделиться маркером</li>
           </ActiveLink>
         </ol>
         <ActiveLink href="/help#search-route">
