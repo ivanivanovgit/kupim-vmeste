a610c597f926e13e8af6cb1878cfa01296c9cabd
Ivan <ivan.ivanov.freelancer@gmail.com>
2023-07-07 19:59:52 +0300

    before deploying

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Head from "next/head";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
-import theme from "../src/theme";
+import theme from "../src/Theme";
 import createEmotionCache from "../src/createEmotionCache";
 import LayOut from "../src/components/Auxiliary/LayOut";
 import "../styles/globals.css";
