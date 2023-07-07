0292d7874f62d6328dcfabc177f6b2991e2ecfaf
Ivan <ivan.ivanov.freelancer@gmail.com>
2023-07-07 20:45:12 +0300

    api/markers.js

--- pages/api/markers.js
+++ pages/api/markers.js
@@ -2,6 +2,11 @@
 import pool from "../../src/utils/db";
 
 export default async (_, res) => {
-  const { rows } = await pool.query("SELECT * FROM markers");
-  res.status(200).json(rows);
+  try {
+    const { rows } = await pool.query("SELECT * FROM markers");
+    res.status(200).json(rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
 };
