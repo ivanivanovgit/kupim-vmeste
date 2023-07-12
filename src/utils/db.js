b61f9cc2511af352780ab62f73367a5a7bcd1bab
Ivan <ivan.ivanov.freelancer@gmail.com>
2023-07-12 16:24:41 +0300

    ElephantSQL

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,7 +3,8 @@ import pg from "pg";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+  /*   connectionString: process.env.POSTGRES_URL + "?sslmode=require", */
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
